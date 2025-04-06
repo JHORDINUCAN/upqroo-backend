3bfa15ea2e87b8d53537d68995a92d8a00120eef
jhordin_ucan <jhordinalexander2015@gmail.com>
2025-04-06 01:28:51 -0500

    Mi backend UPQROO

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/contacto', async (req, res) => {
+  const { email } = req.body;
+
+  if (!email || !email.includes('@')) {
+    return res.status(400).json({ message: 'Correo inválido' });
+  }
+
+  // Configura tu correo institucional (reemplaza los datos reales)
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // o "hotmail", "outlook" o SMTP
+    auth: {
+      user: '202200528@upqroo.edu.mx', // TU CORREO
+      pass: 'cgmj ayql wgyt acmk',
+    },
+  });
+
+  const mailOptions = {
+    from: '202200528@upqroo.edu.mx',
+    to: 'jhordinalexander2015@gmail.com', // CORREO DE DESTINO
+    subject: 'Nuevo correo de contacto desde la app UPQROO',
+    text: `Se registró este correo: ${email}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: 'Correo enviado correctamente' });
+  } catch (error) {
+    console.error('Error al enviar correo:', error);
+    res.status(500).json({ message: 'Error al enviar correo' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
